d76606ddf672fde9fa3e9b57493296b5e3f2ebd2
vishalxdogra <dograv8888@gmail.com>
2026-01-05 12:08:28 +0530

    added dummy server

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # CONFIGURATION FOR JSONPLACEHOLDER
-BASE_URL = "https://postman-echo.com"
+BASE_URL = "http://127.0.0.1:9090"
 REQUEST_TIMEOUT = 10
 TIME_DIFF_THRESHOLD = 2.0  # Low threshold since this API is fast
 MAX_STRING_SIZE = 500
